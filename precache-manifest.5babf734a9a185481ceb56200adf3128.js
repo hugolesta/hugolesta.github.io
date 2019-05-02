c95f56690a44b846c04caa1de45495008f733324
Hugo Noe LESTA <u0162683@naranja.com>
2019-05-02 06:27:33 -0300

    Subiendo portal de noticias

---
+++ precache-manifest.5babf734a9a185481ceb56200adf3128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690f6209c81cdc56d49f6b0125ad32a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59827ead47c5d1a37fe5",
+    "url": "/static/css/main.fb9fa780.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "59827ead47c5d1a37fe5",
+    "url": "/static/js/main.ff98b609.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
